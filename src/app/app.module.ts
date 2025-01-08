82d87f5193ad502e7bea2c5d9fdf02683fa0f2c6
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-01-08 15:19:14 -0300

    Prisma configuration: creating user table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import envValidationSchema from './config/app-env';
+import { UserModule } from 'src/user/user.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   imports: [
@@ -10,6 +12,8 @@ import envValidationSchema from './config/app-env';
       envFilePath: '.env',
       validationSchema: envValidationSchema(),
     }),
+    PrismaModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/app/config/app-env.ts
+++ src/app/config/app-env.ts
@@ -2,5 +2,6 @@ import * as Joi from '@hapi/joi';
 export default () => {
   return Joi.object({
     APP_PORT: Joi.number().default(3000),
+    DATABASE_URL: Joi.string().required(),
   });
 };
