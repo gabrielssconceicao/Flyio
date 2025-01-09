2d46f12afd2e21c0bd0bd3d94ed964d1bfa1ef7f
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-01-09 11:57:17 -0300

    adding defaults collums

---
+++ prisma/migrations/20250109142253_adding_default_values_to_user_columns/migration.sql
@@ -0,0 +1,19 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "bio" TEXT DEFAULT '',
+    "profileImg" TEXT DEFAULT '',
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_User" ("bio", "createdAt", "email", "id", "name", "password", "profileImg", "updatedAt") SELECT "bio", "createdAt", "email", "id", "name", "password", "profileImg", "updatedAt" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
