bf027f1aba442ee8c26bc0abed30b2e8c784a6f9
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-01-10 15:38:02 -0300

    Swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { appDocsSwagger } from './app/config/app-docs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  appDocsSwagger(app);
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
