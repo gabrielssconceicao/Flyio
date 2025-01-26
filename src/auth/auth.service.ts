f5e8e81eda7fc281855af1640cb3f05533133ff8
Gabriel Conceição <gabrielc2019@outlook.com.br>
2025-01-26 11:42:49 -0300

    Auth: checking if the user exists, and if the password is correct

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { HashingServiceProtocol } from './hashing/hashing.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly hashingService: HashingServiceProtocol,
+  ) {}
+  private throwUnauthorizedError() {
+    throw new UnauthorizedException('User or password invalid');
+  }
+  async login(loginDto: LoginDto) {
+    const user = await this.prismaService.user.findFirst({
+      where: {
+        OR: [{ username: loginDto.login }, { email: loginDto.login }],
+      },
+    });
+    if (!user) {
+      throw new this.throwUnauthorizedError();
+    }
+    const isPasswordValid = await this.hashingService.compare(
+      loginDto.password,
+      user.password,
+    );
+    if (!isPasswordValid) {
+      this.throwUnauthorizedError();
+    }
+    console.log(user);
+    return loginDto;
+  }
+}
--- src/auth/bcrypt.service.ts
+++ src/auth/bcrypt.service.ts
@@ -6,4 +6,8 @@ export class BcryptService extends HashingServiceProtocol {
     const salt = await bcrypt.genSalt();
     return bcrypt.hash(password, salt);
   }
+
+  async compare(password: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(password, hash);
+  }
 }
